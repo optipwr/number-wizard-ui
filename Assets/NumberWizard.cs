4562f14faf72fe1a4101e988a8cb5482debef6c6
Michael Irby <michael.irbstoomuch@gmail.com>
2017-08-04 09:20:00 -0400

    final changes

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -18,8 +18,7 @@ public class NumberWizard : MonoBehaviour {
 	void StartGame () {
 		max = 1000;
 		min = 1;
-		guess = 500;
-		max = max + 1;
+		NextGuess();
 	}
 	
 	public void GuessLower(){
@@ -33,7 +32,7 @@ public class NumberWizard : MonoBehaviour {
 	}
 	
 	void NextGuess () {
-		guess = (max + min) / 2;
+		guess = Random.Range(min, max + 1);
 		text.text = guess.ToString();
 		maxGuessesAllowed = maxGuessesAllowed - 1;
 		if(maxGuessesAllowed <= 0){
